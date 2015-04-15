c6a30ee9060a3e38dfdacc6b960c0135fcb2e40b
Katey Basye <kbasye@gmail.com>
2015-04-14 18:53:10 -0700

    Button bar refactor

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
   creationStickyNav();
   mainStickyNav();
   var bb = new ButtonBar({type: 'end'});
-      bb.init();
   var form = new Form();
 
   // TEMPORARY
@@ -16,6 +15,117 @@ $(document).ready(function(){
 
 });
 
+function ButtonBar(settings){
+  var mod = settings.mod || '';
+  var type = settings.type;
+
+  function openBar(){
+    var bar = $($(this).parent());
+        bar.addClass('open');
+        bar.removeClass('closed');
+        $.each(bar.children('.option'), function(i, option){
+          $(option).addClass('option-'+i.toString()).dequeue();
+        })
+    bar.children('.option').hover(function(){
+      $(this).children('.button-label').toggleClass('hidden');
+    })
+    bar.closest('html').on('keyup', function(e){
+      if(e.which == 27){ closeBar(bar); }
+    });
+  }
+
+  function closeBar(bar){
+    bar.removeClass('open');
+    bar.addClass('closed');
+    $.each(bar.children('.option'), function(i, option){
+      $(option).removeClass('option-'+i.toString()).dequeue();
+    })
+  }
+  function createBTW(){
+    html = "<ul class='button-bar button-bar-btw closed'><li class='option item' data-module-type='grid'><button><i class='h2-size fa fa-newspaper-o'></i></button><br><span class='hidden button-label font_small'>Grid</span></li><li class='option item' data-module-type='comparison'><button><i class='h2-size fa fa-cc-visa'></i></button><br><span class='hidden button-label font_small'>Compare</span></li><li class='item option-toggle'><button><i class='h2-size fa fa-plus'></i></button><br><span class='hidden font_small'>Add</span></li><li class='option item' data-module-type='half'><button><i class='h2-size fa fa-bar-chart'></i></button><br><span class='hidden button-label font_small'>Half</span></li><li class='option item' data-module-type='text'><button><i class='h2-size fa fa-file-text'></i></button><br><span class='hidden button-label font_small'>Text</span></li><li class='option item' data-module-type='video'><button><i class='h2-size fa fa-file-video-o'></i></button><br><span class='hidden button-label font_small'>Video</span></li></ul>";
+    el = $($.parseHTML(html));
+    el.attr('id', "btw-bar-" + $('.button-bar-btw').length);
+    return el
+  }
+
+  function init(el){
+    el.on('click', '.option-toggle', openBar);
+    el.on('click', '.option', function(){
+      var mod = new Module($(this));
+          mod.create();
+      closeBar($(this).parent());
+    });
+  }
+
+  function driver(){
+    if(type == 'end'){
+      init($('#end-bar'));
+    }
+    else if(type == 'btw'){
+      var btw = createBTW();
+      btw.insertBefore(mod);
+      init(btw);
+    }
+  }
+  driver();
+}
+function Module(option){
+  var bar = option.parent();
+  var type = option.data('module-type');
+  var modID = type + "-module-" + $('.module').length;
+
+  function comparison(){
+    var html = "<article class='comparison-module module padding-bottom padding-top'><div class='row group wrapper'><div class='six columns'><i class='fa fa-cc-visa'></i><span class='cursor-def'> Comparison</span></div><div class='six columns h-righted'><i class='fa fa-remove a'></i></div></div><div class='dropzone row group wrapper'><p class='caps'>Drop two photos here.</p></div><div class='row group wrapper'><textarea class='tag-input padding-top' placeholder='#tags'></textarea><div></div></div></article>";
+    return html;
+  }
+  function grid(){
+    var header = "<article class='grid-module module padding-bottom padding-top'><div class='row group wrapper'><div class='six columns'><i class='fa fa-photo'></i><span class='cursor-def'> Photo Grid</span></div><div class='six columns h-righted'><i class='fa fa-remove a'></i></div></div>";
+    var html = "<div class='dropzone droppable group wrapper'><p class='caps'>Drag up to ten photos here.</p></div><div class='row group wrapper'><textarea class='tag-input padding-top' placeholder='#tags'></textarea><div></div></div></article>";
+    return header+html;
+  }
+  function half(){
+    var html = "<article class='half-module module padding-bottom padding-top'><div class='row group wrapper'><div class='six columns'><i class='fa fa-bar-chart'></i><span class='cursor-def'> Photo with Text</span></div><div class='six columns h-righted'><i class='fa fa-remove a'></i></div></div><div class='row group wrapper'><div class='text-module h-centered six columns'><textarea class='text-module-body' placeholder='Add some text' class='twelve columns'></textarea></div><div class='droppable dropzone six columns'><p class='caps'>Drag one photo here.</p></div></div><div class='row group wrapper'><textarea class='tag-input padding-top' placeholder='#tags'></textarea><div></div></div></article>";
+    return html;
+  }
+  function text(){
+    var html = "<article class='text-module module padding-bottom padding-top'><div class='row group wrapper'><div class='six columns'><i class='fa fa-file-text'></i><span class='cursor-def'> Text</span></div><div class='six columns h-righted'><i class='fa fa-remove a'></i></div></div><div class='row group wrapper'><textarea class='text-module-body' placeholder='Add some text' class='twelve columns'></textarea></div><div class='row group wrapper'><textarea class='tag-input padding-top' placeholder='#tags'></textarea><div></div></div></article>";
+    return html;
+  }
+  function video(){
+    var html = "<article class='video-module module padding-bottom padding-top'><div class='row group wrapper'><div class='six columns'><i class='fa fa-file-video-o'></i><span class='cursor-def'> Video</span></div><div class='six columns h-righted'><i class='fa fa-remove a'></i></div></div><div class='row group wrapper'><textarea class='padding-bottom' name='text-module-body' placeholder='Insert video URL from Youtube or Vimeo' class='twelve columns'></textarea><textarea class='caption char-limited padding-top' placeholder='Add an optional caption.'></textarea><span class='font_small light hidden' data-limit='140'>140</span></div><div class='row group wrapper'><textarea class='tag-input padding-top' placeholder='#tags'></textarea><div></div></div></article>";
+    return html;
+  }
+  this.create = function(){
+    var html;
+    if(type == 'comparison'){ html = comparison() }
+    else if(type == 'grid'){ html = grid() }
+    else if(type == 'half'){ html = half() }
+    else if(type == 'text'){ html = text() }
+    else if(type == 'video'){ html = video() }
+    var mod = $($.parseHTML(html));
+        mod.attr('id', modID);
+
+    var icon = mod.find('.fa-remove')
+        icon.attr('id', modID);
+        icon.on('click', function(){ 
+          $('#'+modID).remove();
+          $('#btw-bar-'+modID.split('-')[2]).remove();
+        })
+
+    bar.before(mod);
+    new ButtonBar({type: 'btw', mod: mod});
+
+    if(type == 'grid'){
+      var dd = new DragDrop();
+          dd.modDD(mod);
+    }
+    else if(type == 'text' || type == 'half'){
+      new Form($('#'+modID).find('textarea.text-module-body'));
+    }
+    new Form($('#'+modID).find('textarea.caption'));
+    new Form($('#'+modID).find('textarea.tag-input'));
+  }
+}
 function DragDrop(){
   function createPhotoRow(){
     var html="<div class='row group photo-row'></div>";
@@ -160,8 +270,7 @@ function Form(el){
     $('#project-tag-list').keyup(function(e){ appendTag(e); });
     $('#project-title').keyup(function(e){ changeCounter(e); });
   }
-  function init(){
-    console.log('hello')
+  function driver(){
     if(el.hasClass('caption')){
       el.keyup(function(e){ changeCounter(e); });
       autosize(el);
@@ -176,124 +285,7 @@ function Form(el){
       loadDoc()
     }
   }
-  init();
-}
-
-function Module(option){
-  var bar = option.parent();
-  var type = option.data('module-type');
-  var modid = type + "-module-" + $('.module').length;
-
-  function comparison(){
-    var html = "<article class='comparison-module module padding-bottom padding-top'><div class='row group wrapper'><div class='six columns'><i class='fa fa-cc-visa'></i><span class='cursor-def'> Comparison</span></div><div class='six columns h-righted'><i class='fa fa-remove a'></i></div></div><div class='dropzone row group wrapper'><p class='caps'>Drop two photos here.</p></div><div class='row group wrapper'><textarea class='tag-input padding-top' placeholder='#tags'></textarea><div></div></div></article>";
-    return html;
-  }
-  function grid(){
-    var header = "<article class='grid-module module padding-bottom padding-top'><div class='row group wrapper'><div class='six columns'><i class='fa fa-photo'></i><span class='cursor-def'> Photo Grid</span></div><div class='six columns h-righted'><i class='fa fa-remove a'></i></div></div>";
-    var html = "<div class='dropzone droppable group wrapper'><p class='caps'>Drag up to ten photos here.</p></div><div class='row group wrapper'><textarea class='tag-input padding-top' placeholder='#tags'></textarea><div></div></div></article>";
-    return header+html;
-  }
-  function half(){
-    var html = "<article class='half-module module padding-bottom padding-top'><div class='row group wrapper'><div class='six columns'><i class='fa fa-bar-chart'></i><span class='cursor-def'> Photo with Text</span></div><div class='six columns h-righted'><i class='fa fa-remove a'></i></div></div><div class='row group wrapper'><div class='text-module h-centered six columns'><textarea class='text-module-body' placeholder='Add some text' class='twelve columns'></textarea></div><div class='droppable dropzone six columns'><p class='caps'>Drag one photo here.</p></div></div><div class='row group wrapper'><textarea class='tag-input padding-top' placeholder='#tags'></textarea><div></div></div></article>";
-    return html;
-  }
-  function text(){
-    var html = "<article class='text-module module padding-bottom padding-top'><div class='row group wrapper'><div class='six columns'><i class='fa fa-file-text'></i><span class='cursor-def'> Text</span></div><div class='six columns h-righted'><i class='fa fa-remove a'></i></div></div><div class='row group wrapper'><textarea class='text-module-body' placeholder='Add some text' class='twelve columns'></textarea></div><div class='row group wrapper'><textarea class='tag-input padding-top' placeholder='#tags'></textarea><div></div></div></article>";
-    return html;
-  }
-  function video(){
-    var html = "<article class='video-module module padding-bottom padding-top'><div class='row group wrapper'><div class='six columns'><i class='fa fa-file-video-o'></i><span class='cursor-def'> Video</span></div><div class='six columns h-righted'><i class='fa fa-remove a'></i></div></div><div class='row group wrapper'><textarea class='padding-bottom' name='text-module-body' placeholder='Insert video URL from Youtube or Vimeo' class='twelve columns'></textarea><textarea class='caption char-limited padding-top' placeholder='Add an optional caption.'></textarea><span class='font_small light hidden' data-limit='140'>140</span></div><div class='row group wrapper'><textarea class='tag-input padding-top' placeholder='#tags'></textarea><div></div></div></article>";
-    return html;
-  }
-  this.create = function(){
-    var html;
-    if(type == 'comparison'){ html = comparison() }
-    else if(type == 'grid'){ html = grid() }
-    else if(type == 'half'){ html = half() }
-    else if(type == 'text'){ html = text() }
-    else if(type == 'video'){ html = video() }
-    var mod = $($.parseHTML(html));
-        mod.attr('id', modid);
-
-    var icon = mod.find('.fa-remove')
-        icon.attr('id', modid);
-        icon.on('click', function(){ 
-          $('#'+modid).remove();
-          $('#btw-bar-'+modid.split('-')[2]).remove();
-        })
-
-    var btw = new ButtonBar({type: 'btw'});
-        btw.init();
-    bar.before(mod);
-    mod.before(btw.bar);
-
-    if(type == 'grid'){
-      var dd = new DragDrop();
-          dd.modDD(mod);
-    }
-    else if(type == 'text' || type == 'half'){
-      new Form($('#'+modid).find('textarea.text-module-body'));
-    }
-    new Form($('#'+modid).find('textarea.caption'));
-    new Form($('#'+modid).find('textarea.tag-input'));
-  }
-}
-
-function ButtonBar(settings){
-  var type = settings.type;
-  function closeBar(bar){
-    bar.removeClass('open');
-    bar.addClass('closed');
-    $.each(bar.children('.option'), function(i, option){
-      $(option).removeClass('option-'+i.toString()).dequeue();
-    })
-  }
-  function createBTW(){
-    html = "<ul class='button-bar button-bar-btw closed'><li class='option item' data-module-type='grid'><button><i class='h2-size fa fa-newspaper-o'></i></button><br><span class='hidden button-label font_small'>Grid</span></li><li class='option item' data-module-type='comparison'><button><i class='h2-size fa fa-cc-visa'></i></button><br><span class='hidden button-label font_small'>Compare</span></li><li class='item option-toggle'><button><i class='h2-size fa fa-plus'></i></button><br><span class='hidden font_small'>Add</span></li><li class='option item' data-module-type='half'><button><i class='h2-size fa fa-bar-chart'></i></button><br><span class='hidden button-label font_small'>Half</span></li><li class='option item' data-module-type='text'><button><i class='h2-size fa fa-file-text'></i></button><br><span class='hidden button-label font_small'>Text</span></li><li class='option item' data-module-type='video'><button><i class='h2-size fa fa-file-video-o'></i></button><br><span class='hidden button-label font_small'>Video</span></li></ul>";
-    el = $($.parseHTML(html));
-    el.attr('id', "btw-bar-" + $('.button-bar-btw').length);
-    return el
-  }
-  function createBar(){
-    var el;
-    if(type == 'end'){ 
-      el = $('#end-bar');
-    }
-    else if(type == 'btw') {
-      el = createBTW();
-    }
-    return el;
-  }
-  function hide(el){ el.addClass('hidden'); }
-  function show(el){ el.removeClass('hidden'); }
-  function openBar(){
-    var bar = $($(this).parent());
-        bar.addClass('open');
-        bar.removeClass('closed');
-        $.each(bar.children('.option'), function(i, option){
-          $(option).addClass('option-'+i.toString()).dequeue();
-        })
-    bar.children('.option').hover(function(){
-      $(this).children('.button-label').toggleClass('hidden');
-    })
-    bar.closest('html').on('keyup', function(e){
-      if(e.which == 27){ closeBar(bar); }
-    });
-  }
-
-  // Public
-  this.bar = createBar();
-  this.endBar = $('#end-bar');
-  this.options = this.bar.children('.option');
-
-  this.init = function(){
-    this.bar.on('click', '.option-toggle', openBar);
-    this.bar.on('click', '.option', function(){
-      var mod = new Module($(this));
-          mod.create();
-      closeBar($(this).parent());
-    });
-  }
+  driver();
 }
 
 var creationStickyNav = function () {

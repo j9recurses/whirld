53872d1e2b9e59cca4dd8d1aea7904de14ac116e
j9recurses <j9.heiser@ischool.berkeley.edu>
2015-04-16 17:39:18 -0700

    numpy and scipy instructions

--- app/models/user_gallery.rb
+++ app/models/user_gallery.rb
@@ -4,6 +4,7 @@ class UserGallery < ActiveRecord::Base
   has_many :photos, :dependent => :destroy
   validates_presence_of :name
   validates_uniqueness_of :name
+  has_one :photo_grid_order
 
   def at_least_one_photo?
     self.photos.present?

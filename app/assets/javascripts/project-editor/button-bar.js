8bb40c2cbd92cd172dcd57701bd420c135349224
Katey Basye <kbasye@gmail.com>
2015-04-23 00:35:10 -0700

    Modules are appending and creating (ajax). Button Bar is not opening.

---
+++ app/assets/javascripts/project-editor/button-bar.js
@@ -0,0 +1,150 @@
+// button bar needs to open, close
+var ButtonBar = function(options){
+  this.options = $.extend({
+    barType: 'end',
+    barClassName: 'button-bar',
+    barLabelClassName: 'button-label',
+    barToggleClassName: 'option-toggle'
+  }, options);
+
+  // attributes set with initBar()
+  this.id = null;
+  this.barEl = null;
+  this.toggle = null;
+  this.label = null;
+
+  // utility classes
+  this.hide = function(el){ el.addClass('hidden'); }
+  this.show = function(el){ el.removeClass('hidden'); }
+  
+  this.invisible = function(el){ el.addClass('invisible'); }
+  this.visible = function(el){ el.removeClass('invisible'); }
+};
+
+ButtonBar.prototype = {
+  // setters used in initBar()
+  setId: function(){
+    console.log('set ID');
+    var barId = this.options.barType + '-bar';
+    if(this.options.barType == 'end'){
+      return barId;
+    }
+    else{
+      return barId += '-' + $('.' + this.options.barClassName).length;
+    }
+  },
+  setBar: function(){
+    var bar;
+    if(this.options.barType == 'end'){
+      bar = $('#' + this.id);
+      return bar;
+    }
+    else{
+      bar = this.htmlBar();
+      bar.attr('id', this.id);
+      return bar;
+    }
+  },
+  setLabel: function(){
+    console.log('set label');
+    return this.toggle.find('.'+ this.options.barLabelClassName);
+  },
+  setToggle: function(){
+    console.log('toggle');
+    return this.barEl.find('.' + this.options.barToggleClassName)
+  },
+  setData: function(){
+    this.id = this.setId();
+    this.barEl = this.setBar();
+    this.toggle = this.setToggle();
+    this.label = this.setLabel();
+  },
+  // html to render new bar
+  htmlBar: function(){
+    html = "<ul class='button-bar button-bar-btw'><li class='option item invisible' data-mod-type='grid'><button><i class='h2-size fa fa-square'></i></button><br><span class='invisible button-label font_small'>Grid</span></li><li class='option item invisible' data-mod-type='comparison'><button><i class='h2-size fa fa-sliders'></i></button><br><span class='invisible button-label font_small'>Compare</span></li><li class='item option-toggle'><button><i class='h2-size fa fa-plus'></i></button><br><span class='hidden font_small'>Add</span></li><li class='option item invisible' data-mod-type='split'><button><i class='h2-size fa fa-star-half'></i></button><br><span class='invisible button-label font_small'>Split</span></li><li class='option item invisible' data-mod-type='text'><button><i class='h2-size fa fa-file-text'></i></button><br><span class='invisible button-label font_small'>Text</span></li><li class='option item invisible' data-mod-type='video'><button><i class='h2-size fa fa-file-video-o'></i></button><br><span class='invisible button-label font_small'>Video</span></li></ul>";
+    return $(html);
+  },
+
+  // event listeners
+  open:function(){
+    console.log('open bar');
+    console.log(this)
+    var self = this;
+    $.each(this.barEl.children('.option'), function(i, option){
+      self.visible($(option));
+      $(option).addClass('option-'+i).dequeue();
+      $(option).off().on({
+        click: function(){
+          var mod = new Module({ 
+                                  modType: $(option).data('mod-type'),
+                                  originBarId: self.id
+                                });
+              mod.create();
+        },
+        mouseenter: function(){
+          self.visible($(this).find('.button-label'));
+        },
+        mouseleave: function(){
+          self.invisible($(this).find('.button-label'));
+        }
+      })
+    });
+    this.hideToggle();
+  },
+  close: function(){
+    var self = this;
+    console.log('close bar');
+    $.each(self.barEl.children('.option'), function(i, option){
+      self.invisible($(option));
+      $(option).removeClass('option-'+i.toString()).dequeue();
+    });
+    self.showToggle();
+  },
+  showLabel: function(){
+    console.log('show label');
+    this.visible(this.label);
+  },
+  hideLabel: function(){
+    console.log('hide label');
+    this.invisible(this.label);
+  },
+  showToggle: function(){
+    this.show(this.toggle);
+  },
+  hideToggle: function(){
+    this.hide(this.toggle);
+  },
+  removeBar: function(){
+    this.barEl.remove();
+  },
+
+  // run after instantiation
+  initBar: function(){
+    // after things are appended to the DOM, set the information
+    this.setData();
+
+    // preserve context for events
+    var self = this;
+
+    // attach events
+    // hide and show labels, click to open bar
+    this.toggle.off().on({
+      click: function(){
+        self.open();
+      },
+      mouseenter: function(){
+        self.showLabel();
+      },
+      mouseleave: function(){
+        self.hideLabel();
+      }
+    });
+
+    // press esc to close bar
+    self.barEl.closest('html').off().on({
+      keyup: function(e){
+        if(e.which == 27){ self.close(); }
+      }
+    });
+  }
+};
--- app/assets/javascripts/project-editor/refactor.js
+++ app/assets/javascripts/project-editor/refactor.js
@@ -1,158 +1,194 @@
 $(document).ready(function(){
-
-// initiate button bar
-
-
+  // initiate button bar
   var bb = new ButtonBar();
       bb.initBar();
+
 });
 
-
-// button bar needs to open, close
-var ButtonBar = function(options){
+// Constructor for module html
+var ModuleHtml = function(options){
   this.options = $.extend({
-    barType: 'end',
-    barClassName: 'button-bar',
-    barLabelClassName: 'button-label',
-    barToggleClassName: 'option-toggle'
+    modType: 'grid'
+  }, options);
+
+  this.emptyMessage = null;
+  this.iconClass = null;
+  this.label = null;
+
+  this.setModData();
+}
+// Functions for module html and events
+ModuleHtml.prototype = {
+  setModData: function(){
+    if(this.options.modType == 'grid'){
+      this.emptyMessage = 'Drag up to ten photos here.';
+      this.iconClass = 'square';
+      this.label = 'Photo Grid';
+    }
+    else if(this.options.modType == 'split'){
+      this.emptyMessage = "<p class='caps'>Drag up to ten photos here.</p>";
+      this.iconClass = 'star-half';
+      this.label = 'Text With Photo';
+    }
+    else if(this.options.modType == 'comparison'){
+      this.emptyMessage = 'Drag two photos here to compare them.';
+      this.iconClass = 'sliders';
+      this.label = 'Photo Grid';
+    }
+    else if(this.options.modType == 'text'){
+      this.iconClass = 'file-text';
+      this.label = 'Text';
+    }
+    else if(this.options.modType == 'video'){
+      this.iconClass = 'file-video';
+      this.label = 'Video';
+    }
+  },
+  htmlCaption: function(){
+    var html = "<textarea class='caption char-limited padding-top' placeholder='Add an optional caption.'></textarea><span class='char-limit font_small light hidden' data-limit='140'>140</span>";
+    return $(html);
+  },
+  htmlDropzone: function(){
+    var html = "<div class='droppable dropzone row group wrapper'><p class='caps'>" + this.emptyMessage +"</p></div>";
+    return html;
+  },
+  htmlHeader: function(){
+    var html = "<div class='row group wrapper'><div class='six columns'><i class='fa fa-" + this.icon + "'></i><span class='cursor-def'> " + this.options.modType + "</span></div><div class='six columns h-righted'><i class='fa fa-remove a'></i><i class='fa fa-save'></i></div></div>";
+    return html
+  },
+  htmlTaginput: function(){
+    var html = "<div class='row group wrapper'><textarea class='tag-input padding-top' placeholder='#tags'></textarea><div class='tag-container'></div></div>";
+    return html
+  },
+  comparison: function(){
+    var html = "<article class='comparison-module module padding-bottom padding-top'>" + this.htmlHeader() + this.htmlDropzone() + this.htmlTaginput(); + "</article>";
+    return $(html);
+  },
+  grid: function(){
+    var html = "<article class='grid-module module padding-bottom padding-top'>" + this.htmlHeader() + this.htmlDropzone() + this.htmlTaginput() +"</article>";
+    return $(html);
+  },
+  split: function(){
+    var text = "<div class='text-module h-centered six columns'><textarea class='text-module-body' placeholder='Add some text' class='twelve columns'></textarea></div>";
+    var photo = "<div class='droppable dropzone six columns'>" + this.htmlDropzone() + "</div>";
+    var html = "<article class='split-module module padding-bottom padding-top'>" + this.htmlHeader() + "<div class='row group wrapper'>" + photo + text + "</div>" + this.htmlTaginput() + "</article>";
+    return $(html);
+  },
+  text: function(){
+    var html = "<article class='text-module module padding-bottom padding-top'>" + this.htmlHeader() + "<div class='row group wrapper'><textarea class='text-module-body' placeholder='Add some text' class='twelve columns'></textarea></div>" + this.htmlTaginput() + "</article>";
+    return $(html);
+  },
+  video: function(){
+    var html = "<article class='video-module module padding-bottom padding-top'>" + this.htmlHeader() + "<div class='row group wrapper'><textarea class='padding-bottom' class='text-module-body' placeholder='Insert video URL from Youtube or Vimeo' class='twelve columns'></textarea>" + this.htmlCaption() + "</div>" + this.htmlTaginput() + "</article>";
+    return $(html);
+  },
+  html: function(){
+    var modHtml = 'this.' + this.options.modType + '()';
+    return eval(modHtml);
+  }
+}
+
+// Constructor for module logic, ajax
+var Module = function(options){
+  this.options = $.extend({
+    modType: 'grid',
+    modClassName: 'module',
+    originBarId: 'end-bar',
+    removeButtonClassName: 'fa-remove',
+    saveButtonClassName: 'fa-save'
   }, options);
 
   // attributes set with initBar()
   this.id = null;
-  this.bar = null;
-  this.mod = null;
-  this.toggle = null;
-  this.label = null;
-
-  // utility class
-  this.hide = function(el){ el.addClass('invisible'); }
-
-  this.show = function(el){ el.removeClass('invisible'); }
-
-};
-ButtonBar.prototype = {
-  // setters used in initBar()
-  setId: function(){
-    console.log('set ID');
-    var barId = this.options.barType + '-bar';
-    if(this.options.barType == 'end'){
-      return barId;
-    }
-    else{
-      return barId += '-' + $('.' + this.options.barClassName).length;
-    }
-  },
-  setBar: function(){
-    console.log('set bar');
-    var bar;
-    if(this.options.barType == 'end'){
-      bar = $('#' + this.id);
-      console.log(bar)
-      return bar;
-    }
-    else{
-      bar = this.htmlBar();
-      bar.attr('id', this.id);
-      return bar;
-    }
-  },
-  setLabel: function(){
-    console.log('set label');
-    return this.toggle.find('.'+ this.options.barLabelClassName);
-  },
-  setToggle: function(){
-    console.log('toggle');
-    return this.bar.find('.' + this.options.barToggleClassName)
-  },
-
-  // html to render new bar
-  htmlBar: function(){
-    html = "<ul class='button-bar button-bar-btw'><li class='option item invisible' data-module-type='grid'><button><i class='h2-size fa fa-square'></i></button><br><span class='invisible button-label font_small'>Grid</span></li><li class='option item invisible' data-module-type='comparison'><button><i class='h2-size fa fa-sliders'></i></button><br><span class='invisible button-label font_small'>Compare</span></li><li class='item option-toggle'><button><i class='h2-size fa fa-plus'></i></button><br><span class='hidden font_small'>Add</span></li><li class='option item invisible' data-module-type='split'><button><i class='h2-size fa fa-star-half'></i></button><br><span class='invisible button-label font_small'>Split</span></li><li class='option item invisible' data-module-type='text'><button><i class='h2-size fa fa-file-text'></i></button><br><span class='invisible button-label font_small'>Text</span></li><li class='option item invisible' data-module-type='video'><button><i class='h2-size fa fa-file-video-o'></i></button><br><span class='invisible button-label font_small'>Video</span></li></ul>";
-    return $(html);
-  },
-
-  // event listeners
-  open:function(){
-    console.log('open bar');
-    $.each(this.bar.children('.option'), function(i, option){
-      $(option).removeClass('invisible').addClass('option-'+i).dequeue();
-      $(option).off().on({
-        mouseenter: function(){
-          $(this).find('.button-label').removeClass('invisible');
-        },
-        mouseleave: function(){
-          $(this).find('.button-label').addClass('invisible');
-        }
-      })
-    });
-  },
-  close: function(){
-    console.log('close bar');
-    $.each(this.bar.children('.option'), function(i, option){
-      $(option).addClass('invisible').dequeue();
-      $(option).removeClass('option-'+i.toString()).dequeue();
-    });
-  },
-  showLabel: function(){
-    console.log('show label');
-    this.show(this.label);
-  },
-  hideLabel: function(){
-    console.log('hide label');
-    this.hide(this.label);
-  },
-  removeBar: function(){
-    this.bar.remove();
-  },
-  initBar: function(){
-    this.id = this.setId();
-    this.bar = this.setBar();
-    this.toggle = this.setToggle();
-    this.label = this.setLabel();
-
-    var self = this;
-
-    // attach events
-
-    // hide and show labels, click to open bar
-    this.toggle.off().on({
-      click: function(){
-        self.open();
-      },
-      mouseenter: function(){
-        self.showLabel();
-      },
-      mouseleave: function(){
-        self.hideLabel();
-      }
-    });
-
-    // press esc to close bar
-    var self = this;
-    this.bar.closest('html').off().on({
-      keyup: function(e){
-        if(e.which == 27){ self.close(); }
-      }
-    });
-  },
-  createBtwBar: function(mod){
-    this.initBar();
-
-    this.mod = mod;
-    mod.before(this.bar);
-  
-  }
-};
-
-var Module = function(options){
-  this.options = $.extend({
-
-  }, options);
-
-  // needs a bar id to know what to remove when it is removed.
-
+  this.modEl = null;
+  this.originBar = null;
+  this.photoLimit = null;
+  this.removeButton = null;
+  this.saveButton = null;
+  this.user_gallery_id = $('#project-creation-2').data('user-gallery-id');
 
 }
 Module.prototype = {
+  setId: function(){
+    console.log('set module id')
+  },
+  setNewBar: function(){
+    var bar = new ButtonBar({ barType: 'btw' });
+        bar.initBar();
+    return bar;
+  },
+  setOriginBar: function(){
+    var bar = new ButtonBar({ barType: this.options.originBarId.split('-')[0] });
+        bar.initBar();
+    return bar;
+  },
+  setData: function(){
+    if(this.options.modType == 'grid'){
+      this.photoLimit = 10;
+    }
+    else if(this.options.modType == 'split'){
+      this.photoLimit = 1;
+    }
+    else if(this.options.modType == 'comparison'){
+      this.photoLimit = 2;
+    }
+  },
+  setParts: function(){
+    this.modEl = new ModuleHtml({ modType: this.options.modType }).html();
+    this.removeButton = this.modEl.find('.' + this.options.removeButtonClassName);
+    this.saveButton = this.modEl.find('.' + this.options.saveButtonClassName);
+    this.originBar = this.setOriginBar();
+    this.newBar = this.setNewBar();
+  },
+  initBar: function(){
+    // after things are appended to the DOM, set the information and identify its parts
+    this.setData();
+    this.setParts()
 
+    // append module to DOM before the origin bar, before events are attached
+    this.originBar.barEl.before(this.modEl);
+    this.originBar.close();
+
+    // append new bar before module
+    this.modEl.before(this.newBar.barEl);
+
+    // preserve context for events
+    var self = this;
+
+    // attach events
+    this.removeButton.on({
+      click: function(){
+        self.modEl.remove();
+      }
+    });
+
+    this.saveButton.on({
+      click: function(){
+        console.log('save module')
+      }
+    });
+  },
+  create: function(){
+    var url = '/photo_mods/user_gallery_' + this.options.modType + '_create/' + this.user_gallery_id;
+    var data = {user_gallery_id: this.user_gallery_id};
+    $.ajax({
+      url: url,
+      data:  data,
+      cache: false,
+      type: 'post',
+      success: function(data) {
+        console.log('Success: module created');
+        
+        // Set ID
+        this.id = data.id;
+      },
+      error: function(){
+        console.log("Error: module not created");
+      }
+    }); // end ajax
+    this.initBar();
+  },
+  delete: function(){
+
+  }
 }

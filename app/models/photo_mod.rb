30f11222c734128bc683bef6a0add7446eee3d0e
j9recurses <j9.heiser@ischool.berkeley.edu>
2015-04-24 21:25:54 -0700

    quick fix

--- app/models/photo_mod.rb
+++ app/models/photo_mod.rb
@@ -3,7 +3,7 @@ class PhotoMod < ActiveRecord::Base
   belongs_to :photo
 
 def self.get_mod_photos(photo_order)
-    photos = photo_order.split(,)
+    photos = photo_order.split(",")
     photos.each do |photo|
       unless photo.blank?
         photo = PhotoMod.find(photo.id)

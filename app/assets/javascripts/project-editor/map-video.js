1e00ab96cded4efb51bc623500ad2ee3c2ed40fd
Katey Basye <kbasye@gmail.com>
2015-05-03 21:04:45 -0700

    trying to get lines drawn on maps

--- app/assets/javascripts/project-editor/map-video.js
+++ app/assets/javascripts/project-editor/map-video.js
@@ -8,21 +8,6 @@ MapVideo.prototype = {
 
 	init: function(){
 		console.log('Initated map video');
-      // Leaflet Draw. Set up draw bar and create drawnItems, and bind link to them on creation
-      drawnItems = new L.FeatureGroup();
-      map.addLayer(drawnItems);
-      drawControl = new L.Control.Draw({
-      //Can get rid of the markers that we don't want
-            draw: {
-        rectangle: false,
-        polygon: false,
-        marker: false,
-        circle: false
-                  }
-        //Could have options to edit/delete the lines in here. But just took it out
-      });
-      map.addControl(drawControl);
-			console.log(map)
-      console.log(drawControl)
+
 	}
 }

ca1748e42fb06afcf4b223d6e615918b07126d16
j9recurses <j9.heiser@ischool.berkeley.edu>
2015-05-04 05:50:10 -0700

    began user profile and added user activity

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  def after_sign_up_path_for(resource)
-    '/an/example/path'
-  end
+  #def after_sign_up_path_for(resource)
+   # '/an/example/path'
+  #end
 
 
 
